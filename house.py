aa669cb98d51d0d821ffbfc97782202ad880d73c
Matias Andina <matiasandina@gmail.com>
2024-03-25 12:45:19 -0400

    adding init command for sync in house.py

--- house.py
+++ house.py
@@ -4,7 +4,7 @@ import signal
 import time
 
 def house():
-    with LightController(save_data=True, save_mins=5) as lights, Thermometer(save_data=True, save_mins=5) as temp:
+    with LightController(save_data=True, save_mins=5) as lights, Thermometer(save_data=True, save_mins=5, sync_data=True, sync_key_path = 'secret_db_keys.yaml') as temp:
         while True:
             lights.step()
             temp.step()
