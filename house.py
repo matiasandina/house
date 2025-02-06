f55d5b83ebd4d6fa97ffbe560433b0e432805122
Matias Andina <matiasandina@gmail.com>
2025-02-06 09:23:29 -0500

    Isolate each module call to prevent one module from crashing the whole program

--- house.py
+++ house.py
@@ -6,10 +6,18 @@ import time
 def house():
     with LightController(save_data=True, save_mins=5) as lights, Thermometer(save_data=True, save_mins=5, sync_data=True, sync_key_path = 'secret_db_keys.yaml') as temp:
         while True:
-            lights.step()
-            temp.step()
+        # Isolate each module call to prevent one module from crashing the whole program
+            try:
+                lights.step()
+            except Exception as e:
+                print("Error in LightController:", e)
+            
+            try:
+                temp.step()
+            except Exception as e:
+                print("Error in Thermometer:", e)
+            
             time.sleep(1)
-
 
 if __name__ == '__main__':
     house()
